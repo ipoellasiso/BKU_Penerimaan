7694cd9cb4e05a66c1aa6edcb6a9ff89cc27f203
ipoellasiso <ipoellasiso86@gmail.com>
2025-04-23 15:33:28 +0800

    up

--- resources/views/Halaman_Depan/index.blade.php
+++ resources/views/Halaman_Depan/index.blade.php
@@ -39,7 +39,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
-            <marquee scrolldelay='300'><a class="navbar-brand" href="#page-top">Sistem Informasi Penerimaan Pendapatan Daerah Kota Palu (SIPETANDA)</a></marquee>
+            <marquee scrolldelay='380'><a class="navbar-brand" href="#page-top">Sistem Informasi Penerimaan Pendapatan Daerah Kota Palu (SIPETANDA)</a></marquee>
                 <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
